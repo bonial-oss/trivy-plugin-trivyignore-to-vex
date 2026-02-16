4602432bae7b715d5a72fae65476935bb7cd2355
Patrick Jungermann <Patrick.Jungermann@gmail.com>
2026-02-16 10:56:28 +0100

    chore: add stub main entry point

---
+++ main.go
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: 2026 Bonial International GmbH
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Fprintln(os.Stderr, "trivyignore-to-vex: not yet implemented")
+	os.Exit(2)
+}
